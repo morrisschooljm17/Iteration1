754459022abf3a63f0ef04c3f6835b625e8eb64b
Josh Quist <quist127@morris.umn.edu>
2022-04-08 12:51:31 -0500

    Level changes pt 2

--- Assets/Scripts/KillPlane.cs
+++ Assets/Scripts/KillPlane.cs
@@ -5,19 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class KillPlane : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D col)
+    void OnCollisionEnter2d(Collision otherObj)
     {
       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
