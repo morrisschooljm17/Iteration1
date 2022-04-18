4baed17eadac5a39e599ef7db6754bee14fe9e78
Josh Quist <quist127@morris.umn.edu>
2022-04-18 12:09:28 -0500

    Add clicks to doors opening and closing

--- Assets/Scripts/LeverandShut.cs
+++ Assets/Scripts/LeverandShut.cs
@@ -6,9 +6,12 @@ public class LeverandShut : MonoBehaviour
 {
     [SerializeField] private Door[] doorsToOpen;
     [SerializeField] private Door[] doorsToClose;
+    private AudioSource audioSource;
 
 public void activate()
     {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
         for (int i = 0; i < doorsToOpen.Length; i++)
         {
             doorsToOpen[i].openDoor();
